470c282ca08216f3d72fe056224620c33fed9cb9
Michael Rubin <michael.rubin@hagel.ch>
2024-01-03 12:00:33 +0100

    change name

--- manager.R
+++ manager.R
@@ -3,15 +3,14 @@
 # pak::pak("posit-dev/r-shinylive")
 # pak::pak("rstudio/httpuv")
 
-packageVersion("shinylive")
-packageVersion("httpuv")
-
-# Copy "Hello World" from `{shiny}`
-system.file("examples", "01_hello", package="shiny") |>
-  fs::dir_copy("myapp", overwrite = TRUE)
-
+# packageVersion("shinylive")
+# packageVersion("httpuv")
+# 
+# # Copy "Hello World" from `{shiny}`
+# system.file("examples", "01_hello", package="shiny") |>
+#   fs::dir_copy("myapp", overwrite = TRUE)
+# 
 
 shinylive::export(appdir = "myapp", destdir = "docs")
 
 httpuv::runStaticServer("docs/")
-httpuv::runStaticServer("site/")
--- myapp/app.R
+++ myapp/app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
 
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("Hello Michael!"),
 
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -47,7 +47,7 @@ server <- function(input, output) {
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
+    hist(x, breaks = bins, col = "red", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
 
