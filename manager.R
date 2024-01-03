8446e6818449b96907839c72429139335538d464
Michael Rubin <michael.rubin@hagel.ch>
2024-01-03 11:53:19 +0100

    initial test

---
+++ manager.R
@@ -0,0 +1,17 @@
+
+# install.packages("pak")
+# pak::pak("posit-dev/r-shinylive")
+# pak::pak("rstudio/httpuv")
+
+packageVersion("shinylive")
+packageVersion("httpuv")
+
+# Copy "Hello World" from `{shiny}`
+system.file("examples", "01_hello", package="shiny") |>
+  fs::dir_copy("myapp", overwrite = TRUE)
+
+
+shinylive::export(appdir = "myapp", destdir = "docs")
+
+httpuv::runStaticServer("docs/")
+httpuv::runStaticServer("site/")
